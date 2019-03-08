ec7107e1caee4c15853800bd86f659c0fe314667
zhaoyang <18355403288@163.com>
2019-03-08 16:38:21 +0800

    init update

---
+++ src/components/tab_bar/tab_bar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="tab-bar">
+    <div class="tab-wrap">
+      <p class="tab-home" @click="$router.push('/')">
+        <!-- <img src="@/assets/home/tab-ico-home-pre.png" alt> -->
+        <!-- <img v-if="path === '/'" src="@/assets/home/tab-ico-home-pre.png" alt>
+        <img v-else src="@/assets/home/tab-ico-home-nor.png" alt> -->
+        首页
+      </p>
+      <p onclick="alert('null')">
+        <!-- <img src="@/assets/home/tab-ico-community-nor.png" alt> -->
+        社区
+      </p>
+      <p @click="$router.push('my')">
+        <!-- <img src="@/assets/home/tab-ico-me-nor.png" alt> -->
+        <!-- <img v-if="path === '/my'" src="@/assets/home/tab-ico-me-pre.png" alt>
+        <img v-else src="@/assets/home/tab-ico-me-nor.png" alt> -->
+        我的
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { Component, Vue } from "vue-property-decorator";
+
+export default {
+  name: 'tabBar',
+  data() {
+    return {
+      path: this.$route.path
+    };
+  },
+  created() {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tab-bar {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 1rem;
+  border-top: 1px solid #ccc;
+  // opacity: 0.7;
+  font-size: 0.24rem;
+  background-color: #fff;
+  // img {
+  //   width: 0.28rem;
+  // }
+  .tab-wrap {
+    display: flex;
+    p {
+      flex: 1;
+    }
+    img {
+      width: 0.56rem;
+    }
+    .tab-home {
+      
+    }
+  }
+}
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <TabBar></TabBar>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import TabBar from '@/components/tab_bar/tab_bar.vue';
+
+export default {
+  name: 'home',
+  components: {
+    TabBar
+    // HelloWorld
+  }
+}
+</script>
